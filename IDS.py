814c15ee8aa1c5dae170be1fe0d7718453a9e4f6
Muhammad Hashir <149549355+Hashiir01@users.noreply.github.com>
2025-01-08 08:55:54 +0500

    Update IDS.py

--- IDS.py
+++ IDS.py
@@ -4,29 +4,28 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def load():
-    data=pd.read_excel('C:/Users/HP/OneDrive/Desktop/IDS_PROJECT/DATA SET  EMPLOYEE  RETENTION.xlsx')
-    data=data.drop('Asupervisor treated you  with consideration when giving you your performance appraisal results',axis=1)
-    data=data.drop('Employee number',axis=1)
+    data = pd.read_excel('C:/Users/HP/OneDrive/Desktop/IDS_PROJECT/DATA SET  EMPLOYEE  RETENTION.xlsx')
+    data = data.drop('Asupervisor treated you  with consideration when giving you your performance appraisal results', axis=1)
+    data = data.drop('Employee number', axis=1)
     return data
 
-data=load()
-# print(data.shape)
+data = load()
 
 #*****************Rename Columns*************************
-rename= {
-        'I am Enjoying my job': 'Job Enjoyment',
-        'Potential promotion opportunities ': 'Promotion Opportunities',
-        'Good relationship with supervisor /managers': 'Relation with supervisor',
-        'supervisor rated you on how well you did your job, not on his/her personal opinion of you': 'Fair rating',
-        'Sufficient time is allocated for product and solution training': 'Training time',
-        'The supervisor that evaluated you showed concern for your rights as an employee': 'Supervisor Concern',
-        'Overall, how hard did the supervisor who rated your performance try to be fair to you': 'Fairness',
-        'Overall, how fairly were you treated by the supervisor who rated your performance': 'Fair Treatment',
-        'It would be easy to find a job in another department': 'Job Mobility',
-        'My supervisor rewards a good idea by implementing it and giving the responsible employee( s) credit': 'Reward of Ideas',
-        'I am Rewarded whenever I do a good job': 'Reward for good job',
-        'My chances for being promoted are good ': 'Promotion chances'
-    }
+rename = {
+    'I am Enjoying my job': 'Job Enjoyment',
+    'Potential promotion opportunities ': 'Promotion Opportunities',
+    'Good relationship with supervisor /managers': 'Relation with supervisor',
+    'supervisor rated you on how well you did your job, not on his/her personal opinion of you': 'Fair rating',
+    'Sufficient time is allocated for product and solution training': 'Training time',
+    'The supervisor that evaluated you showed concern for your rights as an employee': 'Supervisor Concern',
+    'Overall, how hard did the supervisor who rated your performance try to be fair to you': 'Fairness',
+    'Overall, how fairly were you treated by the supervisor who rated your performance': 'Fair Treatment',
+    'It would be easy to find a job in another department': 'Job Mobility',
+    'My supervisor rewards a good idea by implementing it and giving the responsible employee( s) credit': 'Reward of Ideas',
+    'I am Rewarded whenever I do a good job': 'Reward for good job',
+    'My chances for being promoted are good ': 'Promotion chances'
+}
 data.rename(columns=rename, inplace=True)
 
 st.set_page_config(
@@ -34,16 +33,100 @@ st.set_page_config(
     page_icon="📊", 
 )
 
+#**************Background******************
+background_css = """
+<style>
+    .stApp {
+        background-color: #fbf89f!important;
+    }
+
+    .css-1lcbmhc.e1fqkh3o0, .css-1lcbmhc {
+        background-color: #e5d478 !important;
+    }
+
+    .stApp, .stApp * {
+        color: black !important;
+    }
+
+    .css-1v3fvcr {
+        font-size: 18px;
+        font-weight: bold;
+        color: black !important;
+    }
+
+    .css-17eq0hr {
+        margin-bottom: 10px;
+    }
+</style>
+"""
+st.markdown(background_css, unsafe_allow_html=True)
+
+button_css = """
+<style>
+    div[role='radiogroup'] > label {
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        padding: 10px;
+        margin-bottom: 5px;
+        background-color: #f5eda2;
+        cursor: pointer;
+    }
+
+    div[role='radiogroup'] > label:hover {
+        background-color: #e0d68f;
+    }
+
+    div[role='radiogroup'] > label[data-selected="true"] {
+        background-color: #e0d68f;
+        color: black;
+        font-weight: bold;
+        border: 2px solid black;
+    }
+</style>
+"""
+st.markdown(button_css, unsafe_allow_html=True)
+
+inline_radio_css = """
+<style>
+    div[role='radiogroup'] {
+        display: flex;
+        flex-direction: row; /* Arrange items in a row */
+        gap: 10px; /* Add space between the buttons */
+    }
+    div[role='radiogroup'] > label {
+        margin-right: 10px;
+        padding: 5px 10px;
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        transition: all 0.3s ease; /* Smooth transition for hover and selection */
+    }
+    div[role='radiogroup'] > label:hover {
+        background-color: #f0f0f0;
+        transform: scale(1.1); /* Slightly enlarge on hover */
+    }
+    div[role='radiogroup'] > label[data-selected="true"] {
+        background-color: #83d2d4; /* Updated selected color */
+        font-weight: bold;
+        border-color: #000;
+        transform: scale(1.3); /* Enlarge more when selected */
+    }
+    div[role='radiogroup'] > label > div:first-child {
+        display: none; /* Remove the circle */
+    }
+    .css-1lcbmhc.e1fqkh3o0, .css-1lcbmhc {
+        color: white !important; /* Set sidebar text color to white */
+    }
+</style>
+"""
+st.markdown(inline_radio_css, unsafe_allow_html=True)
+
+
 #**************Header**********************
 header_html = """
     <style>
-        .header-container {
-            width: 100%;
-            background-color: #0E1117;
-            padding: 5px; 
-            margin-bottom: 20px;
-            
-        }
         .header-text {
             font-family: Arial, sans-serif;
             color: white;
@@ -57,9 +140,7 @@ header_html = """
 """
 st.markdown(header_html, unsafe_allow_html=True)
 
-
 #**************Footer**********************
-
 footer_style = """
     <style>
     .footer {
@@ -67,26 +148,23 @@ footer_style = """
         bottom: 0;
         left: 0;
         width: 100%;
-        background-color: #0E1117;
+        background-color: #fbf89f;
         text-align: center;
         padding: 10px;
         font-size: 14px;
         color: #ffffff;
-        z-index: 1000; /* Ensures it stays above other elements */
+        z-index: 1000;
     }
     .main-content {
-        padding-bottom: 50px; /* Adds space for the footer */
+        padding-bottom: 50px;
     }
     </style>
     <div class="footer">
         © 2025 Muhammad Hashiir | All Rights Reserved
     </div>
 """
-
 st.markdown(footer_style, unsafe_allow_html=True)
 st.markdown('<div class="main-content"></div>', unsafe_allow_html=True)
-
-
 
 
 #************* Title *******************************
@@ -95,18 +173,19 @@ st.title('Employee Retention Dataset Analysis')
 #************* SideBar *****************************
 st.sidebar.title('Analysis')
 page = st.sidebar.radio("Select Analysis Page:", [
-    "Overview","Demographics", "Income Analysis", "Retention Insights", "Job Satisfaction", "ML Model"
+    "Overview 📝","Demographics 📊", "Income Analysis 💲", "Retention Insights 🧲", "Job Satisfaction 😊", "ML Model 🧠"
 ])
 
-if page=="Overview":
+if page=="Overview 📝":
     st.header("Dataset Overview")
-    st.write("Shape Of Data")
-    st.write("**Shape of the dataset:**", data.shape)
+    st.write("**Shape Of Dataset** : (419,22)")
+    st.subheader("**Total Reords** : 419")
     st.write('## The Data is Based on Ratings of Employees')
     st.write('**1 means The Lowest Rating and 5 means the Highest Rating except Age and Experience**')
     st.dataframe(data.head(10))
     
-elif page=='Demographics':
+    
+elif page=="Demographics 📊":
     st.header("Demographics Analysis")
     st.subheader("Age Distribution")
     fig, ax = plt.subplots()
@@ -139,7 +218,7 @@ elif page=='Demographics':
     st.pyplot(fig)
     st.write('**Fif 1.4 Shows the Designation of Employees Based on Marital Status.**')
 
-elif page == "Income Analysis":
+elif page == "Income Analysis 💲":
     st.header("Income Analysis")
     st.subheader("Monthly Income by Designation")
     fig, ax = plt.subplots()
@@ -160,7 +239,7 @@ elif page == "Income Analysis":
     st.write("**Fig 1.7  Shows The Distribution of Monthly Income among the Employees**")
 
 
-elif page == "Retention Insights":
+elif page == "Retention Insights 🧲":
     st.header("Retention Insights")
     st.subheader("Designation vs Promotion Oppertunities")
     fig, ax = plt.subplots()
@@ -174,7 +253,7 @@ elif page == "Retention Insights":
     st.pyplot(fig)
     st.write('**Fig 1.9  Shows The Experience of Employees according to Age.**')
 
-elif page == "Job Satisfaction":
+elif page == "Job Satisfaction 😊":
     st.header('Job Satisfaction')
     st.subheader("Job Satisfaction by Job Mobility")
     fig, ax = plt.subplots()
@@ -188,13 +267,13 @@ elif page == "Job Satisfaction":
     st.pyplot(fig)
     st.write('**Fig 2.1 Shows The Job Enjoyment of Employees Based on Reward for Good Job**')
 
-elif page=='ML Model':
+elif page=="ML Model 🧠":
     from sklearn.model_selection import train_test_split
     from sklearn.ensemble import RandomForestClassifier
     from sklearn.metrics import accuracy_score, classification_report
     from sklearn.preprocessing import StandardScaler
 
-    features = ['Age', 'Experience', 'Income', 'Designation', 'Fairness', 'Job Mobility', 'Reward of Ideas']
+    features = ['Age', 'Experience', 'Income', 'Fairness', 'Job Mobility', 'Reward of Ideas']
     X = data[features]
     y = data['Job Enjoyment']  
 
@@ -219,7 +298,6 @@ elif page=='ML Model':
     age = st.slider("Age", int(data['Age'].min()), int(data['Age'].max()), 30)
     experience = st.slider("Experience", int(data['Experience'].min()), int(data['Experience'].max()), 5)
     income = st.slider("Income", int(data['Income'].min()), int(data['Income'].max()), 3)
-    designation = st.selectbox("Designation", sorted(data['Designation'].unique()))
     fairness = st.slider("Supervisor Fairness", 1, 5, 3)
     job_mobility = st.slider("Job Mobility", 1, 5, 3)
     reward_ideas = st.slider("Reward for Ideas", 1, 5, 3)
@@ -228,7 +306,6 @@ elif page=='ML Model':
         'Age': [age],
         'Experience': [experience],
         'Income': [income],
-        'Designation': [designation],
         'Fairness': [fairness],
         'Job Mobility': [job_mobility],
         'Reward of Ideas': [reward_ideas]
